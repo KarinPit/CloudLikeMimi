f8f2ab0cd5f5c1cb3102485e2a08dd1f57e3247f
KarinPit <karinpitlik@gmail.com>
2025-07-08 15:14:17 +0300

    just commented some code in the backend that's not relevant to my project

--- MimiLectures-backend/public/services/order.service.js
+++ MimiLectures-backend/public/services/order.service.js
@@ -1,54 +1,54 @@
 
-import { httpService } from './http.service.js'
-import { utilService } from './util.service.js'
+// import { httpService } from './http.service.js'
+// import { utilService } from './util.service.js'
 
 
 
-export const orderService = {
-    query,
-    getById,
-    save,
-    remove,
-    getEmptyOrder,
-    addOrderMsg
-}
-window.cs = orderService
+// export const orderService = {
+//     query,
+//     getById,
+//     save,
+//     remove,
+//     getEmptyOrder,
+//     addOrderMsg
+// }
+// window.cs = orderService
 
 
-async function query(filterBy) {
-    return httpService.get('order', { params: filterBy });
-}
+// async function query(filterBy) {
+//     return httpService.get('order', { params: filterBy });
+// }
 
-function getById(orderId) {
-    return httpService.get(`order/${orderId}`)
-}
+// function getById(orderId) {
+//     return httpService.get(`order/${orderId}`)
+// }
 
-async function remove(orderId) {
-    return httpService.delete(`order/${orderId}`)
-}
-async function save(order) {
-    var savedOrder
-    if (order._id) {
-        savedOrder = await httpService.put(`order/${order._id}`, order)
+// async function remove(orderId) {
+//     return httpService.delete(`order/${orderId}`)
+// }
+// async function save(order) {
+//     var savedOrder
+//     if (order._id) {
+//         savedOrder = await httpService.put(`order/${order._id}`, order)
 
-    } else {
-        savedOrder = await httpService.post('order', order)
-    }
-    return savedOrder
-}
+//     } else {
+//         savedOrder = await httpService.post('order', order)
+//     }
+//     return savedOrder
+// }
 
-async function addOrderMsg(orderId, txt) {
-    const savedMsg = await httpService.post(`order/${orderId}/msg`, {txt})
-    return savedMsg
-}
+// async function addOrderMsg(orderId, txt) {
+//     const savedMsg = await httpService.post(`order/${orderId}/msg`, {txt})
+//     return savedMsg
+// }
 
 
-function getEmptyOrder() {
-    return {
-        vendor: 'Susita-' + (Date.now() % 1000),
-        price: utilService.getRandomIntInclusive(1000, 9000),
-    }
-}
+// function getEmptyOrder() {
+//     return {
+//         vendor: 'Susita-' + (Date.now() % 1000),
+//         price: utilService.getRandomIntInclusive(1000, 9000),
+//     }
+// }
 
 
 
--- MimiLectures-backend/public/services/user.service.js
+++ MimiLectures-backend/public/services/user.service.js
@@ -5,14 +5,11 @@ const STORAGE_KEY_LOGGEDIN_USER = 'loggedinUser'
 export const userService = {
     login,
     logout,
-    signup,
     getLoggedinUser,
     saveLocalUser,
     query,
     getById,
     remove,
-    update,
-    changeScore
 }
 
 window.userService = userService
@@ -31,13 +28,6 @@ function remove(userId) {
     return httpService.delete(`user/${userId}`)
 }
 
-async function update(user) {
-    user = await httpService.put(`user/${user._id}`, user)
-    // Handle case in which admin updates other user's details
-    if (getLoggedinUser()._id === user._id) saveLocalUser(user)
-    return user
-}
-
 async function login(userCred) {
     const user = await httpService.post('auth/login', userCred)
     if (user) {
@@ -45,28 +35,15 @@ async function login(userCred) {
         return saveLocalUser(user)
     }
 }
-async function signup(userCred) {
-    const user = await httpService.post('auth/signup', userCred)
-    // socketService.login(user._id)
-    return saveLocalUser(user)
-}
+
 async function logout() {
     sessionStorage.removeItem(STORAGE_KEY_LOGGEDIN_USER)
     socketService.logout()
     return await httpService.post('auth/logout')
 }
 
-async function changeScore(by) {
-    const user = getLoggedinUser()
-    if (!user) throw new Error('Not loggedin')
-    user.score = user.score + by || by
-    await update(user)
-    return user.score
-}
-
-
 function saveLocalUser(user) {
-    user = {_id: user._id, fullname: user.fullname, imgUrl: user.imgUrl, score: user.score}
+    user = { _id: user._id, fullname: user.fullname, imgUrl: user.imgUrl, score: user.score }
     sessionStorage.setItem(STORAGE_KEY_LOGGEDIN_USER, JSON.stringify(user))
     return user
 }
@@ -74,13 +51,5 @@ function saveLocalUser(user) {
 function getLoggedinUser() {
     return JSON.parse(sessionStorage.getItem(STORAGE_KEY_LOGGEDIN_USER))
 }
-
-
-// ;(async ()=>{
-    // await userService.signup({fullname: 'Puki Norma', username: 'puki', password:'123',score: 10000, isAdmin: false})
-    // await userService.signup({fullname: 'Master Adminov', username: 'admin', password:'123', score: 10000, isAdmin: true})
-    // await userService.signup({fullname: 'Muki G', username: 'muki', password:'123', score: 10000})
-// })()
-
 
 
